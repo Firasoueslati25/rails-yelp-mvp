7137292cd8d9b74d46e2eea219688643c22b92ad
Firas Oueslati <firas.oueslati@audencia.com>
2021-11-17 16:17:10 +0100

    mmmm

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,12 +2,17 @@ class ReviewsController < ApplicationController
   def new
     # we need @restaurant in our `simple_form_for`
     @review = Review.new
-
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     @review.save
     redirect_to restaurant_path(@restaurant)
   end
+
+    def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to restaurant_path(@review.restaurant)
+    end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,10 @@ Rails.application.routes.draw do
     resources :reviews
   end
 end
+
+Rails.application.routes.draw do
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+  resources :reviews, only: [ :destroy ]
+end
